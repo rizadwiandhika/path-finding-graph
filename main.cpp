6911ac712dcad1c967781a2f0d66c558478205d0
rizadwiandhika <rizadwiandhika@gmail.com>
2021-04-23 18:41:13 +0700

    done: uniformed-cost search

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "/Users/riza/stdc++.h"
+// #include <bits/stdc++.h>
 
 using namespace std;
+
+struct WeightComparator {
+  bool operator ()(const pair<string, int> p1, const pair<string, int> p2) {
+    if (p1.second == p2.second) return p1 < p2;
+    return p1.second < p2.second;
+  }
+};
 
 // Weighted Directed
 class Graph {
@@ -21,9 +29,10 @@ class Graph {
 };
 
 int main() {
-  Graph g(9);
+  const int JUMLAH_NODE = 8;
+  Graph g(JUMLAH_NODE);
 
-  g.add_edge(0, 1, 5);
+  /* g.add_edge(0, 1, 5);
   g.add_edge(0, 2, 2);
   g.add_edge(0, 3, 4);
 
@@ -34,7 +43,70 @@ int main() {
 
   g.add_edge(4, 8, 7);
   g.add_edge(5, 7, 6);
-  g.add_edge(6, 7, 1);
+  g.add_edge(6, 7, 1); */
+
+  /* g.add_edge(0, 1, 3);
+  g.add_edge(0, 4, 4);
+
+  g.add_edge(1, 2, 4);
+  g.add_edge(1, 4, 5);
+  g.add_edge(4, 1, 5);
+  g.add_edge(4, 5, 2);
+
+  g.add_edge(2, 3, 4);
+  g.add_edge(2, 5, 5);
+  g.add_edge(5, 2, 5);
+  g.add_edge(5, 6, 4);
+
+  g.add_edge(6, 7, 3); */
+
+
+  g.add_edge(0, 1, 3);
+  g.add_edge(0, 4, 4);
+
+  g.add_edge(1, 2, 4);
+  g.add_edge(1, 4, 5);
+  g.add_edge(4, 1, 5);
+  g.add_edge(4, 5, 2);
+
+  g.add_edge(2, 3, 4);
+  g.add_edge(2, 5, 5);
+  g.add_edge(4, 5, 2);
+  g.add_edge(1, 2, 4);
+  g.add_edge(5, 2, 5);
+  g.add_edge(5, 6, 4);
+
+  g.add_edge(5, 4, 2);
+  g.add_edge(5, 6, 4);
+  g.add_edge(5, 2, 5);
+  g.add_edge(5, 6, 4);
+  g.add_edge(2, 3, 4);
+  g.add_edge(2, 5, 5);
+  g.add_edge(2, 1, 4);
+  g.add_edge(2, 3, 4);
+  g.add_edge(6, 7, 3);
+
+  g.add_edge(6, 7, 3);
+  g.add_edge(2, 3, 4);
+  g.add_edge(6, 7, 3);
+  g.add_edge(5, 6, 4);
+
+  g.add_edge(6, 7, 3);
+
+
+
+  /* list< pair<string, int> >::iterator it;
+  list< pair<string, int> > tes;
+  tes.push_back(make_pair("Hello", 10));
+  tes.push_back(make_pair("There", 3));
+  tes.push_back(make_pair("World", 5));
+
+  tes.sort(WeightComparator());
+
+  for (it = tes.begin(); it != tes.end(); it++) {
+    cout << it->first << " " << it->second << endl;
+  } */
+  
 
   cout << endl;
 
@@ -117,67 +189,62 @@ void Graph::dfs(int source, int dest) {
 }
 
 string Graph::uniformed_cost(int source, int dest) {
-  list< pair<int, int> >::iterator it;
-  list<string>::iterator itstr;
-  list<string> queue_list;
-  string str_source;
-  string next_path;
+  const int AT_FRONT = 0;
+  bool visited[V];
   int cost_table[V];
+  list< pair<int, int> >::iterator it;
+  list< pair<string, int> >::iterator queuelist_it;
+  list< pair<string, int> > queue_list;
+  pair<string, int> str_source_weight;
 
   for (int i = 0; i < V; i++) {
     cost_table[i] = INT_MAX;
+    visited[i] = false;
   }
 
   cost_table[source] = 0;
-  queue_list.push_back(to_string(source));
+  visited[source] = 0;
+  queue_list.push_back(make_pair(to_string(source), cost_table[source]));
 
   while (!queue_list.empty()) {
-    str_source = queue_list.front();
+    str_source_weight = queue_list.front();
     queue_list.pop_front();
-    source = str_source[0] - '0';
+    source = str_source_weight.first[0] - '0';
 
     cout << "Parent: " << source << endl;
 
     for (it = adj[source].begin(); it != adj[source].end(); it++) {
-      string temp = str_source;
-      temp.insert(0, to_string(it->first));
-      queue_list.push_back(temp);
-
-      char x = it->first - 48 + 64;
-      cout << x << " ";
+      if (visited[it->first]) continue;
 
       int visit_cost = cost_table[source] + it->second;
       if (visit_cost < cost_table[it->first]) cost_table[it->first] = visit_cost;
+
+      string temp = str_source_weight.first;
+      temp.insert(AT_FRONT, to_string(it->first));
+      queue_list.push_back(make_pair(temp, cost_table[it->second]));
+      visited[it->first] = true;
     }
 
-    int next_visit = -1;
-    int next_visit_cost = INT_MAX;
-
-    cout << "queue: ";
-    for (itstr = queue_list.begin(); itstr != queue_list.end(); itstr++) {
-      cout << *itstr << " ";
-      int potential_next_visit = (*itstr)[0] - 48;
-      if (cost_table[potential_next_visit] < next_visit_cost) {
-        next_visit = potential_next_visit;
-        next_visit_cost = cost_table[potential_next_visit];
-        next_path = (*itstr);
-      }
-    }
-    cout << endl;
-
-    queue_list.remove(next_path);
-    queue_list.push_front(next_path);
-
+    // * Harusnya pake priority queue aja
     // Tentuin next visit dari queue list yang cost nya minimum
+    queue_list.sort(WeightComparator());
 
     for (int i = 0; i < V; i++) {
       cout << "Table[" << i << "] = " << cost_table[i] << "\n";
     }
-    cout << "Next path: " << next_path << endl << endl;
 
-    source = next_visit;
-    if (next_visit == dest) break;
+    
+    for (queuelist_it = queue_list.begin(); queuelist_it != queue_list.end(); queuelist_it++) {
+      cout << queuelist_it->first << " ";
+    }
+    cout << endl;
+
+    const int NEXT_VISIT = queue_list.front().first[0] - '0';
+    if (NEXT_VISIT == dest) break;
+
+    cout << "Next path: " << queue_list.front().first << endl;
+    cout << "Next node: " << queue_list.front().first[0] - '0' << endl << endl;
   }
 
-  return next_path;
+  return queue_list.front().first;
 }
