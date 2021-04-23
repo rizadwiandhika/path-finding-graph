b07c6fdc30ca5b84e9cb99b9dc3c6a0e7e57100c
rizadwiandhika <rizadwiandhika@gmail.com>
2021-04-23 23:37:53 +0700

    improve: refactor & fix logical error

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Helper buat sorting list
 struct WeightComparator {
   bool operator ()(const pair<string, int> p1, const pair<string, int> p2) {
     if (p1.second == p2.second) return p1 < p2;
@@ -10,108 +11,67 @@ struct WeightComparator {
   }
 };
 
-// Weighted Directed
+// Weighted Graph (can be directed or undirected)
 class Graph {
   int V;
   bool* visited;
-  bool dfs_goal_accomplished;
   list< pair<int, int> >* adj;
-  stack<int> dfs_stack;
+  
 
   public:
-    string bfs_result;
+    string dfs_result;
 
     Graph(int);
     void add_edge(int, int, int);
-    string bfs(int, int);
-    void dfs(int, int);
-    string uniformed_cost(int, int);
+    void add_edge_undirected(int, int, int);
+    void initialize_dfs();
+    void dfs_search(int, int);
+    string bfs_search(int, int);
+    string uniformed_cost_search(int, int);
 };
 
+
 int main() {
-  const int JUMLAH_NODE = 9;
+  const int JUMLAH_NODE = 8;
+  string result;
+
   Graph g(JUMLAH_NODE);
+  g.add_edge_undirected(0, 1, 3);
+  g.add_edge_undirected(0, 4, 4);
+  g.add_edge_undirected(1, 4, 5);
+  g.add_edge_undirected(1, 2, 4);
+  g.add_edge_undirected(2, 3, 4);
+  g.add_edge_undirected(2, 5, 5);
+  g.add_edge_undirected(4, 5, 2);
+  g.add_edge_undirected(5, 6, 4);
+  g.add_edge_undirected(6, 7, 3);
 
-  g.add_edge(0, 1, 5);
-  g.add_edge(0, 2, 2);
-  g.add_edge(0, 3, 4);
-
-  g.add_edge(1, 4, 9);
-  g.add_edge(1, 5, 4);
-  g.add_edge(2, 7, 6);
-  g.add_edge(3, 6, 2);
-
-  g.add_edge(4, 8, 7);
-  g.add_edge(5, 7, 6);
-  g.add_edge(6, 7, 1);
-
-  /* g.add_edge(0, 1, 3);
-  g.add_edge(0, 4, 4);
-
-  g.add_edge(1, 2, 4);
-  g.add_edge(1, 4, 5);
-  g.add_edge(4, 1, 5);
-  g.add_edge(4, 5, 2);
-
-  g.add_edge(2, 3, 4);
-  g.add_edge(2, 5, 5);
-  g.add_edge(5, 2, 5);
-  g.add_edge(5, 6, 4);
-
-  g.add_edge(6, 7, 3); */
+  result = g.bfs_search(0, 7);
+  cout << "BFS search:" << endl;
+  for (int i = 0; i < result.length(); i++) {
+    if (i == 0) cout << "S";
+    else cout << " -> " << (char)(result[i] - '1' + 'A');
+  }
+  cout << endl << endl;
 
 
-  // g.add_edge(0, 1, 3);
-  // g.add_edge(0, 4, 4);
+  g.initialize_dfs();
+  g.dfs_search(0, 7);
+  result = g.dfs_result;
+  cout << "DFS search:" << endl;
+  for (int i = 0; i < result.length(); i++) {
+    if (i == 0) cout << "S";
+    else cout << " -> " << (char)(result[i] - '1' + 'A');
+  }
+  cout << endl << endl;
 
-  // g.add_edge(1, 2, 4);
-  // g.add_edge(1, 4, 5);
-  // g.add_edge(4, 1, 5);
-  // g.add_edge(4, 5, 2);
-
-  // g.add_edge(2, 3, 4);
-  // g.add_edge(2, 5, 5);
-  // g.add_edge(4, 5, 2);
-  // g.add_edge(1, 2, 4);
-  // g.add_edge(5, 2, 5);
-  // g.add_edge(5, 6, 4);
-
-  // g.add_edge(5, 4, 2);
-  // g.add_edge(5, 6, 4);
-  // g.add_edge(5, 2, 5);
-  // g.add_edge(5, 6, 4);
-  // g.add_edge(2, 3, 4);
-  // g.add_edge(2, 5, 5);
-  // g.add_edge(2, 1, 4);
-  // g.add_edge(2, 3, 4);
-  // g.add_edge(6, 7, 3);
-
-  // g.add_edge(6, 7, 3);
-  // g.add_edge(2, 3, 4);
-  // g.add_edge(6, 7, 3);
-  // g.add_edge(5, 6, 4);
-
-  // g.add_edge(6, 7, 3);
-
-
-
-  /* list< pair<string, int> >::iterator it;
-  list< pair<string, int> > tes;
-  tes.push_back(make_pair("Hello", 10));
-  tes.push_back(make_pair("There", 3));
-  tes.push_back(make_pair("World", 5));
-
-  tes.sort(WeightComparator());
-
-  for (it = tes.begin(); it != tes.end(); it++) {
-    cout << it->first << " " << it->second << endl;
-  } */
-  
-
-  cout << endl;
-
-  string result = g.uniformed_cost(0, 7);
-  cout << result << endl;
+  result = g.uniformed_cost_search(0, 7);
+  cout << "Uniform-cost search:" << endl;
+  for (int i = 0; i < result.length(); i++) {
+    if (i == 0) cout << "S";
+    else cout << " -> " << (char)(result[i] - '1' + 'A');
+  }
+  cout << endl << endl;
 
   return 0;
 }
@@ -120,7 +80,6 @@ Graph::Graph(int V) {
   this->V = V;
   adj = new list< pair<int, int> >[V];
   visited = new bool[V];
-  dfs_goal_accomplished = false;
 
   for (int i = 0; i < V; i++) {
     visited[i] = false;
@@ -131,71 +90,76 @@ void Graph::add_edge(int source, int dest, int weight) {
   adj[source].push_back(make_pair(dest, weight));
 }
 
-string Graph::bfs(int source, int dest) {
-  bool goal_accomplished = false;
-  bool* visited = new bool[V];
-  list<string> queue;
-  string str_source;
-  list< pair<int, int> >::iterator iter;
+void Graph::add_edge_undirected(int source, int dest, int weight) {
+  adj[source].push_back(make_pair(dest, weight));
+  adj[dest].push_back(make_pair(source, weight));
+}
+
+void Graph::initialize_dfs() {
+  dfs_result.clear();
+  for (int i = 0; i < V; i++) {
+    visited[i] = false;
+  }
+}
+
+void Graph::dfs_search(int source, int dest) {
+  list< pair<int, int> >::iterator child_it;
+  visited[source] = true;
+
+  for (child_it = adj[source].begin(); child_it != adj[source].end(); child_it++) {
+    if (visited[dest]) break;
+    if (!visited[child_it->first]) dfs_search(child_it->first, dest);
+  }
+
+  if (visited[dest]) dfs_result.insert(0, to_string(source));
+}
+
+string Graph::bfs_search(int source, int dest) {
+  const int AT_FRONT = 0;
+  bool path_found = false;
+  bool visited[V];
+  list<string> queue_list;
+  list< pair<int, int> >::iterator child_it;
 
   for (int i = 0; i < V; i++) {
     visited[i] = false;
   }
 
-  visited[source] = true;
-  queue.push_back(to_string(source));
+  queue_list.push_back(to_string(source));
 
-  while (!queue.empty() && !goal_accomplished) {
-    str_source = queue.front();
-    queue.pop_front();
-    source = str_source[0] - 48;
+  while (!queue_list.empty() && !path_found) {
+    string source_to_parent_path = queue_list.front();
+    int parent = source_to_parent_path[0] - '0';
+    visited[parent] = true;
+    queue_list.pop_front();
 
-    for (iter = adj[source].begin(); iter != adj[source].end(); iter++) {
-      if (visited[iter->first]) continue;
+    for (child_it = adj[parent].begin(); child_it != adj[parent].end(); child_it++) {
+      int child = child_it->first;
+      if (visited[child]) continue;
 
-      cout << iter->first << " ";
+      string copy_path = source_to_parent_path;
+      copy_path.insert(AT_FRONT, to_string(child));
+      queue_list.push_back(copy_path);
 
-      string temp = str_source;
-      temp.insert(0, to_string(iter->first));
-      queue.push_back(temp);
-      visited[iter->first] = true;
-
-      if (iter->first == dest) {
-        goal_accomplished = true;
+      if (child == dest) {
+        path_found = true;
         break;
       }
     }
   }
-  cout << endl;
 
-  return queue.back();
+  string result = queue_list.back();
+  reverse(result.begin(), result.end());
+  return result;
 }
 
-void Graph::dfs(int source, int dest) {
-
-
-  list< pair<int, int> >::iterator it;
-  visited[source] = true;
-
-  for (it = adj[source].begin(); it != adj[source].end(); it++) {
-    if (visited[dest]) break;
-    if (!visited[it->first]) {
-      cout << it->first << " ";
-      dfs(it->first, dest);
-    }
-  }
-
-  if (visited[dest]) bfs_result.insert(0, to_string(source));
-}
-
-string Graph::uniformed_cost(int source, int dest) {
+string Graph::uniformed_cost_search(int source, int dest) {
   const int AT_FRONT = 0;
-  bool visited[V];
   int cost_table[V];
-  list< pair<int, int> >::iterator it;
-  list< pair<string, int> >::iterator queuelist_it;
+  bool visited[V];
+  list< pair<int, int> >::iterator child_it;
   list< pair<string, int> > queue_list;
-  pair<string, int> str_source_weight;
+  pair<string, int> parent_path_and_weight;
 
   for (int i = 0; i < V; i++) {
     cost_table[i] = INT_MAX;
@@ -206,44 +170,35 @@ string Graph::uniformed_cost(int source, int dest) {
   queue_list.push_back(make_pair(to_string(source), cost_table[source]));
 
   while (!queue_list.empty()) {
-    str_source_weight = queue_list.front();
+    parent_path_and_weight = queue_list.front();
+    int parent = parent_path_and_weight.first[0] - '0';
+    visited[parent] = true;
     queue_list.pop_front();
-    source = str_source_weight.first[0] - '0';
-    visited[source] = true;
 
-    cout << "Parent: " << source << endl;
+    for (child_it = adj[parent].begin(); child_it != adj[parent].end(); child_it++) {
+      int child = child_it->first;
+      int parent_to_child_cost = child_it->second;
 
-    for (it = adj[source].begin(); it != adj[source].end(); it++) {
-      if (visited[it->first]) continue;
+      if (visited[child]) continue;
 
-      int visit_cost = cost_table[source] + it->second;
-      if (visit_cost < cost_table[it->first]) cost_table[it->first] = visit_cost;
+      int source_to_child_cost = cost_table[parent] + parent_to_child_cost;
+      if (source_to_child_cost < cost_table[child]) {
+        cost_table[child] = source_to_child_cost;
+      }
 
-      string temp = str_source_weight.first;
-      temp.insert(AT_FRONT, to_string(it->first));
-      queue_list.push_back(make_pair(temp, cost_table[it->first]));
+      string parent_path = parent_path_and_weight.first;
+      parent_path.insert(AT_FRONT, to_string(child));
+      queue_list.push_back(make_pair(parent_path, cost_table[child]));
     }
 
-    // * Harusnya pake priority queue aja
     // Tentuin next visit dari queue list yang cost nya minimum
     queue_list.sort(WeightComparator());
 
-    for (int i = 0; i < V; i++) {
-      cout << "Table[" << i << "] = " << cost_table[i] << "\n";
-    }
-
-    
-    for (queuelist_it = queue_list.begin(); queuelist_it != queue_list.end(); queuelist_it++) {
-      cout << queuelist_it->first << "(w:" << queuelist_it->second << ") ";
-    }
-    cout << endl;
-
-    const int NEXT_VISIT = queue_list.front().first[0] - '0';
-    if (NEXT_VISIT == dest) break;
-
-    cout << "Next path: " << queue_list.front().first << endl;
-    cout << "Next node: " << queue_list.front().first[0] - '0' << endl << endl;
+    int next_visit = queue_list.front().first[0] - '0';
+    if (next_visit == dest) break;
   }
 
-  return queue_list.front().first;
+  string result = queue_list.front().first;
+  reverse(result.begin(), result.end());
+  return result;
 }
